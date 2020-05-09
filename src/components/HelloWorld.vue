05a2f6ae72d3db0ef9c8e5c7c96b8ef6bd6526e8
lakshman <lakshmanmurugesan@gmail.com>
2020-05-09 22:59:28 +0530

    initial files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h2>EC2 security groups</h2>
+    <!-- Loop region name, list of permissions -->
+    <div v-for="(inbound, inboundIndex) in inboundRules" :key="inboundIndex">
+      <h4>Region - {{ inbound.region }}</h4>
+      <p v-if="inbound.rules.length === 1">No rules found</p>
+      <ul class="inbound-rules">
+        <li
+          :class="{'card': rule.IpProtocol === 'tcp', 'highlight': recommendations(rule).length}"
+          v-for="(rule, ruleIndex) in inbound.rules" :key="ruleIndex">
+          <!-- show only tcp related rules -->
+          <div v-if="rule.IpProtocol === 'tcp'">
+            <div>
+              Protocol: {{ rule.IpProtocol }}
+            </div>
+            <div>
+              Port: {{ displayPort(rule) }}
+            </div>
+            <div v-if="rule.IpRanges && rule.IpRanges.length">
+              IP: {{ rule.IpRanges[0].CidrIp }}
+            </div>
+            <div v-if="rule.Ipv6Ranges && rule.Ipv6Ranges.length">
+              IPV6: {{ rule.Ipv6Ranges[0].CidrIpv6 }}
+            </div>
+            <div class="inbound-rules__recommendations" v-if="recommendations(rule).length">
+              <b>Recommendations</b>:
+              <span v-for="(msg, recommendationIndex) in recommendations(rule)" :key="recommendationIndex">
+                <p>{{ msg }}</p>
+              </span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import AWS from 'aws-sdk';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      inboundRules: [],
+      allRegions: []
+    };
+  },
+  computed: {
+  },
+  methods: {
+    loadUserCredential() {
+      return AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      });
+    },
+    loadUserRegion(region) {
+      return new AWS.EC2({
+        region: region
+      });
+    },
+    displayPort (rule) {
+      if (rule.FromPort === rule.ToPort) {
+        return rule.FromPort;
+      } else {
+        return `${rule.FromPort}-${rule.ToPort}`;
+      }
+    },
+    recommendations (rule) {
+      let infos = [];
+      if (rule.IpRanges && rule.IpRanges.length && rule.IpRanges[0].CidrIp === '0.0.0.0/0') {
+        infos.push('IP has localhost reference, please specify desired ip');
+      }
+      if (rule.Ipv6Ranges && rule.Ipv6Ranges.length && rule.Ipv6Ranges[0].CidrIpv6 === '::/0') {
+        infos.push('IPV6 has localhost reference, please specify desired ip');
+      }
+      if (rule.FromPort === 0 && rule.ToPort === 65535) {
+        infos.push('All ports are public, please specify desired ports');
+      }
+      return infos;
+    }
+  },
+  async mounted() {
+    this.loadUserCredential();
+    let ec2Region = this.loadUserRegion('ap-south-1');
+    await ec2Region.describeRegions( (regionErr, regionData) => {
+      if (regionErr) {
+        console.log(regionErr);
+      } else {
+        regionData.Regions.forEach((region) => {
+          if (region.RegionName) {
+            let ec2SecurityGroups = this.loadUserRegion(region.RegionName);
+            ec2SecurityGroups.describeSecurityGroups((securityGroupErr, securityGroupData) => {
+              if (securityGroupErr) {
+                console.log('error', securityGroupErr);
+              } else {
+                if (securityGroupData && securityGroupData.SecurityGroups && securityGroupData.SecurityGroups[0] && securityGroupData.SecurityGroups[0].IpPermissions) {
+                  this.inboundRules.push({
+                    region: region.RegionName ,
+                    rules: securityGroupData.SecurityGroups[0].IpPermissions
+                  });
+                }
+              }
+            });
+          }
+        });
+      }
+    });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.inbound-rules {
+  margin-left: 35%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.inbound-rules li {
+ list-style: none;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.card {
+  list-style: none;
+  background: gray;
+  width: 400px;
+  margin-bottom: 20px;
+  box-shadow: 4px 4px lightgray;
+  text-align: left;
+  padding-left: 20px;
+  padding-top: 10px;
 }
-a {
-  color: #42b983;
+
+.highlight {
+  border: 3px solid red;
+}
+
+.inbound-rules__recommendations {
+  padding-top: 10px;
+  padding-bottom: 10px;
 }
 </style>
