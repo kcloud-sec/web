36e28cb5fd137744bdca72178fd9b3036cf72f4f
lakshman <lakshmanmurugesan@gmail.com>
2020-05-23 22:00:27 +0530

    Add pagination, scss file

--- src/modules/dashboard/dashboard.vue
+++ src/modules/dashboard/dashboard.vue
@@ -1,8 +1,19 @@
+<style lang="scss">
+  @import "_styles/dashboard";
+</style>
+
 <template>
-  <div>
-    <b-table :items="items" :fields="fields" striped responsive="sm">
+  <div class="dashboard">
+    <h3 class="title">Dashboard</h3>
+    <b-table
+      :items="items"
+      :fields="fields"
+      striped
+      :per-page="perPage"
+      :current-page="currentPage"
+      responsive="sm">
       <template v-slot:cell(show_details)="row">
-        <b-button size="sm" @click="showDetails(row)" class="mr-2">
+        <b-button size="sm" @click="showDetails(row, row.detailsShowing)" class="mr-2">
           {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
         </b-button>
       </template>
@@ -15,19 +26,27 @@
         </b-card>
       </template>
     </b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table" />
   </div>
 </template>
 
 <script>
-import { BTable } from 'bootstrap-vue';
+import { BTable, BPagination } from 'bootstrap-vue';
 
 export default {
   name: 'dashboard',
   components: {
-    'b-table': BTable
+    'b-table': BTable,
+    'b-pagination': BPagination
   },
   data() {
     return {
+      currentPage: 1,
+      perPage: 2,
       rule: {},
       fields: ['rule', 'service', 'categories', 'show_details'],
       items: [
@@ -35,17 +54,26 @@ export default {
         { id: 2, rule: 'AWS AMI Encryption', service: 'EC2', categories: 'Security', _showDetails: false },
         { id: 3, rule: 'Account Instance Limit', service: 'EC2', categories: 'Security', _showDetails: false },
         { id: 4, rule: 'App-Tier EC2 Instance Using IAM Roles', service: 'EC2', categories: 'Security', _showDetails: false },
-        { id: 5, rule: 'App-Tier Publicly Shared AMI', service: 'EC2', categories: 'Security', _showDetails: false }
+        { id: 5, rule: 'App-Tier Publicly Shared AMI', service: 'EC2', categories: 'Security', _showDetails: false },
+        { id: 6, rule: 'Publicly Shared AMI', service: 'EC2', categories: 'Security', _showDetails: false }
       ]
     }
   },
+  computed: {
+    rows () {
+      return this.items.length;
+    }
+  },
   methods: {
-    showDetails (row) {
+    showDetails (row, detailShowing) {
       row.toggleDetails();
-      this.items.forEach(item => {
-        console.log(item.id, row.item.id);
-        return (item.id !== row.item.id) ? item._showDetails = false : row.item._showDetails = true;
-      });
+      if (!detailShowing) {
+        this.items.forEach(item => {
+          return (item.id !== row.item.id) ? item._showDetails = false : row.item._showDetails = true;
+        });
+      } else {
+        row.item._showDetails = false;
+      }
     }
   }
 };
