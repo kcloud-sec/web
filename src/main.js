7a3189fdda8d9d00d7c53ba2048f96df62d29dbe
lakshman <lakshmanmurugesan@gmail.com>
2020-05-23 21:44:29 +0530

    Add data table statically

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes';
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 const router = new VueRouter({mode: 'history', routes});
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 
--- src/modules/dashboard/dashboard.vue
+++ src/modules/dashboard/dashboard.vue
@@ -1,11 +1,52 @@
 <template>
   <div>
-    dashboard
+    <b-table :items="items" :fields="fields" striped responsive="sm">
+      <template v-slot:cell(show_details)="row">
+        <b-button size="sm" @click="showDetails(row)" class="mr-2">
+          {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+        </b-button>
+      </template>
+
+      <template v-slot:row-details="row">
+        <b-card>
+          <b-row class="mb-2">
+            <b-col>{{ row.item }}</b-col>
+          </b-row>
+        </b-card>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+import { BTable } from 'bootstrap-vue';
+
 export default {
-  name: 'dashboard'
+  name: 'dashboard',
+  components: {
+    'b-table': BTable
+  },
+  data() {
+    return {
+      rule: {},
+      fields: ['rule', 'service', 'categories', 'show_details'],
+      items: [
+        { id: 1, rule: 'AMI Naming Conventions', service: 'EC2', categories: 'Security', _showDetails: false },
+        { id: 2, rule: 'AWS AMI Encryption', service: 'EC2', categories: 'Security', _showDetails: false },
+        { id: 3, rule: 'Account Instance Limit', service: 'EC2', categories: 'Security', _showDetails: false },
+        { id: 4, rule: 'App-Tier EC2 Instance Using IAM Roles', service: 'EC2', categories: 'Security', _showDetails: false },
+        { id: 5, rule: 'App-Tier Publicly Shared AMI', service: 'EC2', categories: 'Security', _showDetails: false }
+      ]
+    }
+  },
+  methods: {
+    showDetails (row) {
+      row.toggleDetails();
+      this.items.forEach(item => {
+        console.log(item.id, row.item.id);
+        return (item.id !== row.item.id) ? item._showDetails = false : row.item._showDetails = true;
+      });
+    }
+  }
 };
 </script>
