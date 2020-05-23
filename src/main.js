5314cefecf630169630b77f494a370ba9d0b6e72
lakshman <lakshmanmurugesan@gmail.com>
2020-05-23 12:59:50 +0530

    Add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './routes';
 
-Vue.config.productionTip = false
+const router = new VueRouter({mode: 'history', routes});
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount('#app');
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Dashboard from './modules/dashboard/dashboard';
+
+const routes = [
+  { path: '/dashboard', component: Dashboard }
+];
+
+export default routes;
